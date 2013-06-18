54b95ebbe1d409810e798c2728f46dd5433737e5
Carl-Johan Blomqvist <carl@cjblomqvist.com>
2013-06-18 18:58:10 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+expect-almost
+=====================
+
+Extension to expect.js according to https://github.com/magwo/expect.js/commit/105caeadbd375aeb041ccdeee88ffc55a58fe2e3
---
+++ index.js
@@ -0,0 +1,40 @@
+module.exports = function (expect) {
+  /**
+     * Checks if the obj almost equals another, optionally providing a max absolute difference.
+     *
+     * @api public
+     */
+  expect.Assertion.prototype.approximately = expect.Assertion.prototype.approximatelyEqual = expect.Assertion.prototype.almostEqual = function (obj, maxDiff) {
+    var allowedDiff = maxDiff
+        ;
+
+    if(typeof(allowedDiff)=="undefined") {
+      allowedDiff = 0.0001;
+    }
+
+    allowedDiff = Math.abs(allowedDiff);
+
+    return this.within(obj - allowedDiff, obj + allowedDiff);
+  };
+
+  /**
+     * Assert within start to finish (inclusive). 
+     *
+     * @param {Number} start
+     * @param {Number} finish
+     * @api public
+     */
+  expect.Assertion.prototype.within = function (start, finish) {
+    var range = start + '..' + finish
+        ;
+
+    this.assert(
+        this.obj >= start && this.obj <= finish
+      , 'expected ' + i(this.obj) + ' to be within ' + range
+      , 'expected ' + i(this.obj) + ' to not be within ' + range);
+
+    return this;
+  };
+
+  return expect;
+}
